1aea7c186f2c8d56e24ce3bb05c6451b5dd2f137
dahir yusuf <dahir.yusuf@hotmail.com>
2022-01-06 23:36:54 +0000

    finished game

--- game.rb
+++ game.rb
@@ -1,17 +1,49 @@
-class Generate_questions
+require './game'
+require './score'
+require './questions'
+
+class New_game
+  
   def initialize
-  @num1 = 0
-  @num2 = 0
+    @player_1 = Scores.new()
+    @player_2 = Scores.new()
+    @question = Generate_questions.new()
   end
-  def question
-  @num1 = rand(20)
-  @num2 = rand(20)
-  "what is #{@num1} + #{@num2}"
+  def player1
+    puts "Player 1: #{@question.question}"
+    choice = gets.chomp.to_i
+    if choice != @question.answer
+      @player_1.lost
+      puts "Player 1: Seriously! No!"
+    else
+      puts "Player 1: Yay! you got it"
+    end
   end
-
-  def answer
-    answer = @num1 + @num2
-    answer
+  def player2
+    puts "Player 2: #{@question.question}"
+    choice = gets.chomp.to_i
+    if choice != @question.answer
+      @player_2.lost
+      puts "Player 2: Seriously! No!"
+    else
+      puts "Player 2: Yay! you got it"
+    end
   end
+  def gameover
+    if @player_1.score == 0 || @player_2.score == 0
+      return false
+    else
+      return true
+    end
+  end
+  def score
+    puts "P1: #{@player_1.score}/3 P2: #{@player_2.score}/3"
+  end
+  def winner
+    if @player_1.score == 0
+      return "Player 2 wins with a score of #{@player_2.score}/3"
+    elseif @player_2.score == 0 
+    return "Player 1 wins with a score of #{@player_1.score}/3"
+    end
+  end  
 end
-
--- main.rb
+++ main.rb
@@ -1,52 +1,6 @@
 require './game'
 require './score'
-
-class New_game
-  
-  def initialize
-    @player_1 = Scores.new()
-    @player_2 = Scores.new()
-    @question = Generate_questions.new()
-  end
-  def player1
-    puts "Player 1: #{@question.question}"
-    choice = gets.chomp.to_i
-    if choice != @question.answer
-      @player_1.lost
-      puts "Player 1: Seriously! No!"
-    else
-      puts "Player 1: Yay! you got it"
-    end
-  end
-  def player2
-    puts "Player 2: #{@question.question}"
-    choice = gets.chomp.to_i
-    if choice != @question.answer
-      @player_2.lost
-      puts "Player 2: Seriously! No!"
-    else
-      puts "Player 2: Yay! you got it"
-    end
-  end
-  def gameover
-    if @player_1.score == 0 || @player_2.score == 0
-      return false
-    else
-      return true
-    end
-  end
-  def score
-    puts "P1: #{@player_1.score}/3 P2: #{@player_2.score}/3"
-  end
-  def winner
-    if @player_1.score == 0
-      return "Player 2 wins with a score of #{@player_2.score}/3"
-    elseif @player_2.score == 0 
-    return "Player 1 wins with a score of #{@player_1.score}/3"
-    end
-  end  
-end
-
+require './questions'
 
 class Play_game
   
